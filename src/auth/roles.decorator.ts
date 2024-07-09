a88734a23bc97b69154aebb3f2f6aeed57dc6596
blueclorox <dehydrate92@gmail.com>
2024-07-09 16:47:40 +0900

    기존 모듈 삭제
    비교용

--- src/auth/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Role } from 'src/user/types/userRole.type';
-
-import { SetMetadata } from '@nestjs/common';
-
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
--- src/show/types/show-category.type.ts
+++
@@ -1,8 +0,0 @@
-export enum Category {
-  Musical,
-  Concert,
-  Sports,
-  Classic,
-  Family,
-  Theater,
-}
