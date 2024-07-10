493dcc868ab348f3b1ac2dd26127ca9fed5d5cc4
blueclorox <dehydrate92@gmail.com>
2024-07-10 14:22:45 +0900

    해설 12강 적용
    swagger 사용법
    nestjs 홈페이지 Open API에서 확인 가능

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,14 @@ async function bootstrap() {
       //forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Sparta Node.js TS')
+    .setDescription('Document for Sparta API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('DB_PORT'));
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -20,12 +20,20 @@ export class User {
   @PrimaryGeneratedColumn({ unsigned: true })
   id: number;
 
+  /**
+   * 이메일
+   * @example "example@example.com"
+   */
   @IsNotEmpty({ message: '이메일을 입력해 주세요.' })
   //여기 확인: 그냥 메시지 쓰면 안되는데 앞에 객체 추가하면 됨 ????
   @IsEmail({}, { message: '이메일 형식에 맞지 않습니다.' })
   @Column({ unique: true })
   email: string;
 
+  /**
+   * 비밀번호
+   * @example "EX$ample!!"
+   */
   @IsNotEmpty({ message: '비밀번호를 입력해 주세요.' })
   @IsStrongPassword()
   // select 기본값 지정은 find로 가져왔을때에만 영향을 준다
